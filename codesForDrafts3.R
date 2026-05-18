669ab9d6125d2205b08173382dbb941e20a2638d
lallen-student <lallendelice@umass.edu>
2026-05-17 23:07:08 -0400

    edits

---
+++ codesForDrafts3.R
@@ -0,0 +1,115 @@
+
+# clean memory ------------------------------------------------------------
+rm(list = ls())
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+# read in data ------------------------------------------------------------
+#set working directory
+
+location="https://github.com/lallen-student/HW3/raw/main/"
+
+file= "ncesdata_MASS_202425.xlsx"
+
+link=paste0(location,file)
+
+download.file(link,
+              destfile = file,
+              mode = "wb")
+
+mydata <- readxl::read_xlsx(file, skip = 11)
+
+# see data ----------------------------------------------------------
+
+
+head(mydata)
+
+glimpse(mydata)
+
+summary(mydata)
+
+
+# see data types ----------------------------------------------------------
+
+str(mydata)
+
+# deliverable 1 categorical variable/column ----------------------------------------------------------
+
+
+cat_data <- mydata |> 
+  mutate(
+    locale_group = case_when(
+      grepl("City", Locale) ~ "City",
+      grepl("Suburban", Locale) ~ "Suburban",
+      grepl("Town", Locale) ~ "Town",
+      grepl("Rural", Locale) ~ "Rural")) |> 
+  mutate(
+    size_group = case_when(
+      grepl("Large", Locale) ~ "Large",
+      grepl("Midsize", Locale) ~ "Midsize",
+      grepl("Small", Locale) ~ "Small",
+      TRUE ~ "Other"
+    )
+  ) |>
+  count(locale_group, size_group)
+
+totals <- cat_data |>
+  group_by(locale_group) |>
+  summarize(total = sum(n))
+
+ggplot(cat_data,
+       aes(x = reorder(locale_group, n),
+           y = n,
+           fill = size_group)) +
+  
+  geom_col() +
+  
+  geom_text(data = totals,
+            aes(x = locale_group,
+                y = total,
+                label = total),
+            vjust = -0.5,
+            inherit.aes = FALSE) + 
+  
+  labs(
+    title = "Amount of Massachusetts Schools by Locale Group",
+    x = "Locale Type",
+    y = "Number of Schools",
+    fill = "Locale Size",
+    caption = "Source: NCES Massachusetts School Data"
+  ) +
+  
+  theme_minimal()
+
+# save del1Draft ----------------------------------------------------------
+
+saveRDS(cat_data, file = "del1Draft.rds")
+
+# deliverable 2 numerical variable/column --------------------------------
+
+num_data <- mydata |>
+  filter(Students != "†",
+         Students != "–")
+
+ggplot(num_data,
+       aes(x = as.numeric(Students))) +
+  
+  geom_histogram(
+    bins = 30,
+    fill = "lightblue",
+    color = "black"
+  ) +
+  
+  labs(
+    title = "Distribution of Student Enrollment in Massachusetts",
+    x = "Amount of Students",
+    y = "Amount of Schools",
+    caption = "Source: NCES Massachusetts School Data"
+  ) +
+  
+  theme_minimal()
+
+# save del2Draft ----------------------------------------------------------
+
+saveRDS(num_data, file = "del2Draft.rds")
